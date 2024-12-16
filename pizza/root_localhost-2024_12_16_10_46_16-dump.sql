317eceae9e058d57017f85334946ab449f79b305
alexia-mrn <alexia.mrn87000@gmail.com>
2024-12-16 14:12:09 +0100

    RenduFinalControle
    hehe

--- pizza/root_localhost-2024_12_16_10_46_16-dump.sql
+++ pizza/root_localhost-2024_12_16_10_46_16-dump.sql
@@ -27,7 +27,7 @@ CREATE TABLE `habitante` (
   `label` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `label` (`label`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `habitante` (
 
 LOCK TABLES `habitante` WRITE;
 /*!40000 ALTER TABLE `habitante` DISABLE KEYS */;
-INSERT INTO `habitante` VALUES (29,' Alpine'),(10,' Alsacienne'),(26,' Angevine'),(19,' Aquitaine'),(1,' Auvergnate'),(30,' Azuréenne'),(3,' Bourguignonne'),(5,' Bretonne'),(11,' Champenoise'),(8,' Corse'),(4,' Franc-Comtoise'),(16,' Francilienne'),(9,' Grandestoise'),(13,' Hauts-Française'),(23,' Languedocienne'),(25,' Ligérienne'),(21,' Limousine'),(12,' Lotharingienne'),(24,' Midi-Pyrénéenne'),(18,' Néo-Aquitaine'),(15,' Nordiste'),(17,' Normande'),(22,' Occitane'),(14,' Picarde'),(20,' Poitou-Charentaise'),(28,' Provençale'),(2,' Rhônalpine'),(7,' Val-de-Loirienne'),(27,' Vendéenne'),(33,'Allemande'),(6,'Centraise'),(37,'Despacito'),(31,'Espagnole'),(39,'Mazzirati'),(35,'Polonaise');
+INSERT INTO `habitante` VALUES (40,''),(29,' Alpine'),(10,' Alsacienne'),(26,' Angevine'),(19,' Aquitaine'),(1,' Auvergnate'),(30,' Azuréenne'),(3,' Bourguignonne'),(5,' Bretonne'),(11,' Champenoise'),(8,' Corse'),(4,' Franc-Comtoise'),(16,' Francilienne'),(9,' Grandestoise'),(13,' Hauts-Française'),(23,' Languedocienne'),(25,' Ligérienne'),(21,' Limousine'),(12,' Lotharingienne'),(24,' Midi-Pyrénéenne'),(18,' Néo-Aquitaine'),(15,' Nordiste'),(17,' Normande'),(22,' Occitane'),(14,' Picarde'),(20,' Poitou-Charentaise'),(28,' Provençale'),(2,' Rhônalpine'),(7,' Val-de-Loirienne'),(27,' Vendéenne'),(33,'Allemande'),(6,'Centraise'),(37,'Despacito'),(31,'Espagnole'),(39,'Mazzirati'),(35,'Polonaise');
 /*!40000 ALTER TABLE `habitante` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +57,7 @@ CREATE TABLE `ingredient` (
   UNIQUE KEY `label_commercial` (`label_commercial`),
   KEY `id_typeingredient` (`id_typeingredient`),
   CONSTRAINT `ingredient_ibfk_1` FOREIGN KEY (`id_typeingredient`) REFERENCES `typeingredient` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,9 +66,23 @@ CREATE TABLE `ingredient` (
 
 LOCK TABLES `ingredient` WRITE;
 /*!40000 ALTER TABLE `ingredient` DISABLE KEYS */;
-INSERT INTO `ingredient` VALUES (3,'Base Blanche','Blanco',1),(4,'Tomate','Tomato',2),(5,'Carottes',NULL,2),(6,'Base Tomate','Rosso',1),(7,'Anchois',NULL,2),(8,'Olives',NULL,2),(9,'Jambon Serrano',NULL,2),(12,'Poivrons','Peperone',2),(18,'Champignons','Funghi',2);
+INSERT INTO `ingredient` VALUES (10,'Ricotta','Rico',2),(11,'Emmental','Emmental',2),(12,'Champignons','Champignons',3),(13,'Poivrons','Poivrons',3),(14,'Oignons','Oignons',3),(15,'Tomates','Tomates',3),(16,'Olives','olives',3),(17,'Courgette','Courgette',3),(18,'Mais','Mais',3),(19,'Pepperoni','Pepperoni',5),(20,'Jambon','jambon',5),(21,'Poulet','Poulet',5),(22,'Viande','Viande',5),(23,'Lardons','Lardons',5),(24,'Chorizo','Chorizo',5),(25,'Salami','Salami',5),(26,'Anchois','Anchois',5),(27,'Tempeh','Tempeh',5),(28,'Tofu','Tofu',5),(29,'Origan','Origan',4),(30,'Basilic','Basilic',4),(31,'Thym','Thym',4),(32,'Persil','Persil',4),(33,'Ail','Ail',4),(34,'Piment','Piment',4),(35,'Huile d\'olive','Huile',6),(36,'Miel','Miel',6),(37,'Vianigre','Vinaigre',6),(38,'Noix','Noix',6),(39,'Truffe','truffe',6),(40,'Base Blanche','Blanco',1),(41,'Tomate','Tomato',1),(42,'Pesto','Pesto',1),(43,'Barbecue','Barbecue',1),(44,'Mozza','Mozza',2),(45,'Parmesan','Parm',2),(46,'Gorgonzola','Gorg',2),(47,'Cheddar','Cheddar',2),(48,'Feta','Feta',2);
 /*!40000 ALTER TABLE `ingredient` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `pizza_ingredients_with_type`
+--
+
+DROP TABLE IF EXISTS `pizza_ingredients_with_type`;
+/*!50001 DROP VIEW IF EXISTS `pizza_ingredients_with_type`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `pizza_ingredients_with_type` AS SELECT 
+ 1 AS `ingredient_id`,
+ 1 AS `ingredient`,
+ 1 AS `type`*/;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `size`
@@ -107,7 +121,7 @@ CREATE TABLE `typeingredient` (
   `label` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `label` (`label`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +130,7 @@ CREATE TABLE `typeingredient` (
 
 LOCK TABLES `typeingredient` WRITE;
 /*!40000 ALTER TABLE `typeingredient` DISABLE KEYS */;
-INSERT INTO `typeingredient` VALUES (1,'Base'),(2,'Crudité');
+INSERT INTO `typeingredient` VALUES (1,'Base'),(4,'Epice'),(6,'Extra'),(2,'Fromage'),(3,'Legume'),(5,'Proteine');
 /*!40000 ALTER TABLE `typeingredient` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,9 +171,44 @@ BEGIN
     INSERT INTO ingredient (label, label_commercial, id_typeingredient)
     VALUES (label, label_commercial, id_typeingredient);
 END ;;
-DELIMITER ;CREATE DEFINER=`root`@`localhost` PROCEDURE `AddHabitante`(IN label VARCHAR(255))
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `AddTypeIngredient` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AddTypeIngredient`(IN label VARCHAR(255))
 BEGIN
-    INSERT INTO habitante (label) VALUES (label);
+    INSERT INTO typeingredient (label)
+    VALUES (label);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `DeleteIngredient` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteIngredient`(IN ingredient_id INT)
+BEGIN
+    DELETE FROM ingredient WHERE id = ingredient_id;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -184,46 +233,25 @@ DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `AddTypeIngredient` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `AddTypeIngredient`(IN label VARCHAR(255))
-BEGIN
-    INSERT INTO typeingredient (label)
-    VALUES (label);
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `DeleteIngredient` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `DeleteIngredient`(IN ingredient_id INT)
-BEGIN
-    DELETE FROM ingredient WHERE id = ingredient_id;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `pizza_ingredients_with_type`
+--
+
+/*!50001 DROP VIEW IF EXISTS `pizza_ingredients_with_type`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `pizza_ingredients_with_type` AS select `i`.`id` AS `ingredient_id`,`i`.`label` AS `ingredient`,`ti`.`label` AS `type` from (`ingredient` `i` left join `typeingredient` `ti` on((`i`.`id_typeingredient` = `ti`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -234,7 +262,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-02 13:55:44
-CREATE VIEW pizza_ingredients_with_type AS
-SELECT i.id as ingredient_id, i.label as ingredient, ti.label as type
-FROM ingredient i LEFT JOIN typeingredient ti on i.id_typeingredient = ti.id;
+-- Dump completed on 2024-12-16 10:46:16
